ec955806b721ec13d26d10e0cbbc431a1efda7e1
lilytaub <ltaub@intersystems.com>
2019-06-04 14:36:57 -0400

    graph all mgstat metrics at once, put in a single html file

--- MgstatVis.py
+++ MgstatVis.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 import math
 import pandas as pd
@@ -19,25 +17,29 @@ parser.add_argument("file")
 args = parser.parse_args()
 mgstatfile = args.file
 
+##put mgstat data into a dataframe, index on date
 data = pd.read_csv(mgstatfile,header=1,parse_dates=[[0,1]])
 data.columns = data.columns.str.strip()
 data=data.rename(columns={'Date_       Time':'DateTime'})
 data.index = data.DateTime
 
-#for debugging:
-#list(data)
-
+##output file for bokeh plots
 output_file(os.getcwd()+"/mgstatvis.html")
+
+##tools
 TOOLS="pan,box_zoom,reset,save"
 
-left = figure(tools=TOOLS,x_axis_type="datetime",title="Jrn Writes", width=600,height=350,
-             x_axis_label="time")
-right = figure(tools=TOOLS,x_axis_type="datetime", title="Phys Writes", width=600,height=350,
-              x_axis_label="time",x_range=left.x_range)
-left.line(data.index,data.Jrnwrts,legend="JrnWrts",line_width=1)
-right.line(data.index,data.PhyWrs,legend="PhyWrs",line_width=1)
-p=gridplot([[left,right]])
-show(p)
+## list of plots of each mgstat metric
+plots = []
+for name in data:
+	myplot = figure(tools=TOOLS,x_axis_type="datetime",title=name,
+					width=600,height=350,x_axis_label="time")
+	myplot.line(data.index,data[name],legend=name,line_width=1)
+	##add plot to list
+	plots.append(myplot)
+
+##show all plots
+show(gridplot(plots,ncols=2,merge_tools=True))
 
 
 
